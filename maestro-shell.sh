1501606d3349f18f4955f76710f89ef62feef115
Nic Costa <nic.costa@gmail.com>
2021-07-02 16:22:47 +0000

    Revert "Utility Scripts"
    This reverts commit dc08025de84f5cfc88507a627661037830a1b9a4.
    
    Removing support for grease because we're migrating to a different
    logger called fluentbit.

--- maestro-shell.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ue
-. /etc/profile.d/envvars.sh
-cd /home/vagrant/work/gostuff/src/github.com/armPelionEdge/maestro-shell
-#exec /home/vagrant/work/gostuff/bin/maestro
-exec ./maestro-shell
